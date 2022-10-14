ca334943c5b9709a3fc48fd4cf201b408d567607
Sergey <travoltaj237@gmail.com>
2022-10-14 19:00:52 +0600

    [Update] New addresses and ABI added

--- src/config/constants/tabs.js
+++ src/config/constants/tabs.js
@@ -4,17 +4,17 @@ import stakingFactoryABI from '../abis/stakingFactory.json'
 
 export const tabs = [
     {
-        address: '0xcF00533ad18d86f00d843BEdc105cCa5b5A129CE',
+        address: '0xbd96ae2af4b50540b532d5e492166c7fd3b8b3a5',
         name: 'Multisign Contract',
         abi: multisignABI.abi
     },
     {
-        address: '0x32bE07FB9dBf294c2e92715F562f7aBA02b7443A',
+        address: '0xf68b8d3fae7feb747cb4dce0a4c91a100b140245',
         name: 'Farming Contract',
         abi: farmingABI.abi
     },
     {
-        address: '0xeB81FCD447957d2AE1fBE7676A4684442b9aE941',
+        address: '0x3c1442981f9f14b07a05b4a6fb0c825a8167a8bd',
         name: 'Staking Factory Contract',
         abi: stakingFactoryABI.abi
     },
